63a4c5cafea061c563fbf63fd8967b00dc47dec9
Mykola Lahernyi <lagernuy88@gmail.com>
2023-09-04 13:16:32 +0300

    add fixtures

--- hillel_test_api/test_for_api.py
+++ hillel_test_api/test_for_api.py
@@ -1,36 +1,39 @@
-from hillel_api import *
 import pytest
+from hillel_api import *
+from conftest import *
+
+# pytest less23/test_for_api_fixtures.py
 
 
-def test_sigin_positive():
-
-    user_data = {
-    "email": "qam2608@2022test.com",
-    "password": "Qam2608venv",
-    "remember": False
+def test_reset_password(registered_user):
+    reset_data = {
+        "email": "notmail@test.com"
     }
-    r = auth.signin(s, user_data)
-    r_json = after_processsing(r)    
-    assert r.status_code == 200, "Wrong status code"
-    assert r_json["status"] == "ok", "Key 'status' is not ok"
+    r = auth.resetpassword(s, reset_data)
 
-
-def test_sigin_negative():
-
-    user_data_negative = {
-    "email": "qam@2022test.com",
-    "password": "Qam2",
-    "remember": False
-    }
-    r = auth.signin(s, user_data_negative)
+def test_get_users_profile_positive(logged_user):
+    r = users.profile_get(s)
     r_json = after_processsing(r)
-    assert r.status_code == 400, "Wrong status code"
-    assert r_json["status"] == "error", "Key 'status' is not error"
-
-
-def test_logout():
-
-    r = auth.logout(s)
-    r_json = after_processsing(r)    
     assert r.status_code == 200, "Wrong status code"
-    assert r_json["status"] == "ok", "Key 'status' is not ok"
+    assert r_json["status"] == "ok", "'status' is not ok"
+    assert r_json["data"], "Recieved no user data"
+
+def test_get_cars(logged_user, create_car):
+
+    r = cars.cars_get(s)
+    r_json = after_processsing(r)
+
+    assert r.status_code == 200, "Wrong status code"
+    assert r_json["status"] == "ok", "'status' is not 'ok'"
+    assert len(r_json["data"]) == 1, "Wrong amount of cars received"
+
+def test_get_instructions_positive(instructions_data):
+    assert len(instructions_data) > 0
+
+
+def test_get_user_settings(logged_user):
+    r = users.settings_get(s)
+    r_json = after_processsing(r)
+    assert r.status_code == 200, "Wrong status code"
+    assert r_json["status"] == "ok", "'status' is not ok"
+    assert r_json["data"], "Recieved no user data"
